13b6a4d0a6c8a4a9611cb5af11786f7fc7b8fa78
Adriano Gil <adrianogil.san@gmail.com>
2025-08-30 21:24:52 -0400

    feat: add function to describe CloudFormation stack resource info

---
+++ aws_cloudformation.sh
@@ -0,0 +1,21 @@
+function aws-cloudformation-describe-stack-get-resource-info() {
+    RESOURCE_LOGICAL_ID=$1
+    STACK_NAME=$2
+
+    if [ -z "$RESOURCE_LOGICAL_ID" ]; then
+        echo "Usage: aws-cloudformation-describe-stack-resource <RESOURCE_LOGICAL_ID> [STACK_NAME]"
+        return 1
+    fi
+
+    if [ -z "$STACK_NAME" ]; then
+        # Get all stack names and select one using fuzzy finder
+        STACK_NAME=$(aws cloudformation describe-stacks --query "Stacks[].StackName" --output json | jq -r '.[]' | default-fuzzy-finder)
+    fi
+
+    echo "Fetching resource info for LogicalResourceId '$RESOURCE_LOGICAL_ID' in stack '$STACK_NAME'..."
+
+    RESULT=$(aws cloudformation describe-stack-resources \
+      --stack-name "$STACK_NAME" \
+      --query "StackResources[?LogicalResourceId=='$RESOURCE_LOGICAL_ID']" --output json)
+    echo "$RESULT"
+}
