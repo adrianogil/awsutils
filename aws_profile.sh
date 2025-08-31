80559aadc9d8bc1609f26ae34fad8835d4353651
Adriano Gil <adrianogil.san@gmail.com>
2025-08-30 21:22:04 -0400

    fix: improve aws-profile-set function and enhance output messages

--- aws_profile.sh
+++ aws_profile.sh
@@ -1,12 +1,14 @@
-function aws-profile-set()
-{
+function aws-profile-set () {
     target_aws_profile=$1
-    if [ -z $target_aws_profile ]; then
-        # Get aws profile from ~/.aws/credentials using fuzzy search
-        target_aws_profile=$(cat ~/.aws/credentials | grep -oP '\[.*\]' | tr -d '[]' | default-fuzzy-finder)
+    if [ -z "$target_aws_profile" ]
+    then
+        # Use awk to extract profiles from ~/.aws/credentials
+        target_aws_profile=$(awk -F '[][]' '/\[.*\]/ {print $2}' ~/.aws/credentials | default-fuzzy-finder)
     fi
     export AWS_PROFILE=$target_aws_profile
-    echo $AWS_PROFILE
+    echo "AWS profile set to $AWS_PROFILE"
+    # set region
+    region=$(aws configure get region --profile $AWS_PROFILE)
 }
 alias aps="aws-profile-set"
 
@@ -16,3 +18,10 @@ function aws-profile()
     echo $AWS_PROFILE
 }
 alias ap="aws-profile"
+
+function aws-list-profiles()
+{
+    awk -F '[][]' '/\[.*\]/ {print $2}' ~/.aws/credentials
+}
+alias alp="aws-list-profiles"
+
